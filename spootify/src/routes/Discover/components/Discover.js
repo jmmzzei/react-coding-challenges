64a54907b000181140cd8cb0086e7aaad300920f
José María <josemmazzei@gmail.com>
2021-03-07 19:01:44 -0300

    Add spootify challenge

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,27 +1,70 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from 'react'
+import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock'
+import '../styles/_discover.scss'
+import makeRequest from '../api/makeRequest'
 
 export default class Discover extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
-    };
+      categories: [],
+    }
+  }
+
+  // Se hacen las peticiones asíncronas en el componentDidMount para
+  // evitar re renderizados.
+  async componentDidMount() {
+    // Se utiliza el bloque try catch para manejar los posibles fallos
+    // en las peticiones.
+    try {
+      // Se hace uso del object destructuring
+      const {
+        data: { albums },
+      } = await makeRequest('new-releases')
+
+      const {
+        data: { playlists },
+      } = await makeRequest('featured-playlists')
+
+      const {
+        data: { categories },
+      } = await makeRequest('categories')
+
+      this.setState({
+        playlists: playlists.items || [],
+        newReleases: albums.items || [],
+        categories: categories.items || [],
+      })
+    } catch (e) {
+      alert('An error has ocurred. Try again later.')
+    }
   }
 
   render() {
-    const { newReleases, playlists, categories } = this.state;
+    const { newReleases, playlists, categories } = this.state
 
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        <DiscoverBlock
+          text="RELEASED THIS WEEK"
+          id="released"
+          data={newReleases}
+        />
+        <DiscoverBlock
+          text="FEATURED PLAYLISTS"
+          id="featured"
+          data={playlists}
+        />
+        <DiscoverBlock
+          text="BROWSE"
+          id="browse"
+          data={categories}
+          imagesKey="icons"
+        />
       </div>
-    );
+    )
   }
 }
